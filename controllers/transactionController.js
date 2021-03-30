1aea588eed62249b7a7a1c61dc3bedda95e604f6
Jordy Medina <medinajordy@hotmail.com>
2021-03-30 15:45:56 -0500

    validar valor dif de KIOSKO o CAJA

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -31,21 +31,15 @@ exports.inicializar= async (req,res) =>{
         data.usuarioI=new Date()
         if(req.headers['application']=="UEhBTlRPTVhfV0VC" && req.headers['idorganizacion']=="365509c8-9596-4506-a5b3-487782d5876e"){
             
-            
-            if(req.body==null){
-                response.code=400; 
-                response.success=false; response.message="El campo no es valido."
-                res.send(response);
-                
+            if(!req.body){                response.code=400;     response.success=false; response.message="El campo no es valido.";    res.status(400).send(response);
             }
             else{
                 data.secuencia=req.body.secuenciaUsuario;
-                
-                
                 if(!req.body.nemonicoCanalFacturacion)console.log("d")
                 else if (req.body.nemonicoCanalFacturacion=='CAJA')   data.nemo=nemo.CAJA
                 else if (req.body.nemonicoCanalFacturacion=='KIOSKO')   data.nemo=nemo.KIOSKO
-                //console.log("Si hay data")
+                else{  response.code=400; response.message="El campo no es valido.";  response.data="No Data found"; res.status(400).send(response)      }
+                
                 try {
                     var connection = await oracledb.getConnection(config);
 
@@ -70,7 +64,7 @@ exports.inicializar= async (req,res) =>{
                                     +"(:0, :1, :2, :3, :4, :5, :6, :7, :8, :9)",
                                     [data.codigoPre, data.codigoE, data.secuencia, data.codigoU, data.tipo, data.nemo, data.activo, data.secuencia, data.codigoU, data.usuarioI],{ autoCommit: true } ); //values(21,1,968,'ENEVAREZ','FACTURA',1,'S',968,'ENEVAREZ',SYSDATE)');
                                     response.code=200; response.success=true; response.message=resp; response.Data=["idPreTransaccion ="+data.codigoPre]
-                                    res.send(response);
+                                    res.status(200).send(response);
                                     connection.close()
                                 }
                                 else{
@@ -79,34 +73,34 @@ exports.inicializar= async (req,res) =>{
                                     resp= await connection.execute("INSERT into latino_owner.Fac_Pre_Transacciones(CODIGO_PRE_TRANSACCION,CODIGO_EMPRESA,CODIGO_SUCURSAL,CODIGO_CAJA,NUMERO_PUNTO_EMISION,SECUENCIA_USUARIO,CODIGO_USUARIO,TIPO_PRE_TRANSACCION,CODIGO_CANAL_FACTURACION,ES_ACTIVO,SECUENCIA_USUARIO_INGRESO,USUARIO_INGRESO,FECHA_INGRESO) values "   //(CODIGO_PRE_TRANSACCION,CODIGO_EMPRESA,SECUENCIA_USUARIO,CODIGO_USUARIO,TIPO_PRE_TRANSACCION,CODIGO_CANAL_FACTURACION,ES_ACTIVO,SECUENCIA_USUARIO_INGRESO,USUARIO_INGRESO,FECHA_INGRESO)
                                     +"(:0, :1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12)",
                                     [data.codigoPre, data.codigoE,req.body.caja.codigoSucursal,req.body.caja.codigoCaja,req.body.caja.numeroPuntoEmision ,data.secuencia, data.codigoU, data.tipo, data.nemo, data.activo, data.secuencia, data.codigoU, data.usuarioI],{ autoCommit: true } ); //values(21,1,968,'ENEVAREZ','FACTURA',1,'S',968,'ENEVAREZ',SYSDATE)');
-                                    response.code=200; response.success=true; response.message=resp; response.Data=["idPreTransaccion ="+data.codigoPre]
-                                    res.send(response);
+                                    response.code=200; response.success=true; response.message=resp; response.Data=["idPreTransaccion ="+data.codigoPre]; response.data=[]
+                                    res.status(200).send(response);
                                     connection.close()
                                 }
                             }
                             ;
                             
                         }
-                        else{connection.commit(); connection.release(); response.code=500; response.message="Ha ocurrido un error inesperado.";  resp.data=resp; res.send(response); }
+                        else{connection.commit(); connection.close(); response.code=500; response.message="Ha ocurrido un error inesperado.";  resp.data=resp; res.status(500).send(response); }
 
 
                     }
-                    else{connection.commit(); connection.release(); response.code=500; response.message="Ha ocurrido un error inesperado.";  resp.data="No Data founded"; res.send(response); }
+                    else{connection.commit(); connection.close(); response.code=500; response.message="Ha ocurrido un error inesperado.";  resp.data="No Data found"; res.status(500).send(response); }
                     
                     
                     
 
                  } catch (err) {
-                     console.log('not connected to database'); 
+                     //console.log('not connected to database'); 
                      response.Data=[err.message]
-                     res.send(response)
+                     //res.send(response)
                  } 
                 
                 
             }
         }else{
             response.code=401; response.success=false; response.message="Las cedenciales de autenticación no son válidas."
-            res.send(response)
+            res.status(401).send(response)
         }
     }
 
